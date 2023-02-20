d0327264aea55f82e56a324ee44c50f58f674b1c
Joel Reyes <joel.rys98@gmail.com>
2023-02-20 00:46:38 -0600

    Agregar archivos de proyecto.

---
+++ PM2E18259/PM2E18259/App.xaml.cs
@@ -0,0 +1,46 @@
+﻿using PM2E18259.Controllers;
+using System;
+using System.IO;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PM2E18259
+{
+    public partial class App : Application
+    {
+        static EmpleDB basedatos;
+
+        public static EmpleDB BaseDatos
+        {
+            get
+            {
+                if (basedatos == null)
+                {
+                    basedatos = new EmpleDB(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "EmpleDB.db3"));
+                }
+                return basedatos;
+            }
+        }
+
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new MainPage());
+
+
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ PM2E18259/PM2E18259/Views/MapaEmple.xaml.cs
@@ -0,0 +1,58 @@
+﻿using Plugin.Geolocator;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Maps;
+using Xamarin.Forms.Xaml;
+
+namespace PM2E18259.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MapaEmple : ContentPage
+    {
+        public MapaEmple()
+        {
+            InitializeComponent();
+        }
+
+        protected override async void OnAppearing()
+        {
+            String latitudGuardada = lblLat.Text;
+            String longitudGuardada = lblLon.Text;
+            String descripcionGuardada = lblDesc.Text;
+
+
+
+            base.OnAppearing();
+            Pin ubicacion = new Pin();
+            ubicacion.Label = "Tu destino";
+            ubicacion.Address = descripcionGuardada;
+            ubicacion.Position = new Position(Convert.ToDouble(latitudGuardada), Convert.ToDouble(longitudGuardada));
+            Mapa.Pins.Add(ubicacion);
+
+
+            Mapa.MoveToRegion(new MapSpan(new Position(Convert.ToDouble(latitudGuardada), Convert.ToDouble(longitudGuardada)), 1, 1));
+
+
+
+            var localizacion = CrossGeolocator.Current;
+            if (localizacion != null)
+            {
+                localizacion.PositionChanged += localizacion_positionChanged;
+
+
+                if (!localizacion.IsListening)
+                {
+                    await localizacion.StartListeningAsync(TimeSpan.FromSeconds(10), 100);
+                }
+            }
+        }
+
+        private void localizacion_positionChanged(object sender, Plugin.Geolocator.Abstractions.PositionEventArgs e)
+        {
+            var posicion_mapa = new Position(e.Position.Latitude, e.Position.Longitude);
+            Mapa.MoveToRegion(new MapSpan(posicion_mapa, 1, 1));
+        }
+
+        
+    }
+}
